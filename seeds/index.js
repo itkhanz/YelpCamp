e6858339f2412255cfadb5c2583d428618ad8dfc
itkhan <itkhanz@gmail.com>
2021-06-13 21:29:15 +0200

    Campgrounds CRUD

---
+++ seeds/index.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+//GRAB RANDOM ELEMENT FROM AN ARRAY
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    //CREATE LOOP FOR DATA - 50 times - 50 cities
+    for(let i = 0; i < 50; i++){
+        //random number to pick city[from 1000 city array]
+        const random1000 = Math.floor(Math.random() * 1000);
+        //make new Campground - location: city, state
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        });
+        await camp.save()
+    }
+};
+
+//seedDB returns a promise because it is an async function .then--> close connection
+seedDB().then(() => {
+    mongoose.connection.close();
+});
