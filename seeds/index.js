8200b7ac93661e88aaafff9feb468770e97ebf0d
itkhan <itkhanz@gmail.com>
2021-06-15 14:03:55 +0200

    Finish basic styles and layouts bootstrap

--- seeds/index.js
+++ seeds/index.js
@@ -23,12 +23,17 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     //CREATE LOOP FOR DATA - 50 times - 50 cities
     for(let i = 0; i < 50; i++){
+        //random number for price
+        const price = Math.floor(Math.random() * 20) + 10;
         //random number to pick city[from 1000 city array]
         const random1000 = Math.floor(Math.random() * 1000);
         //make new Campground - location: city, state
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, eius perspiciatis cumque earum voluptatibus beatae. Cumque aperiam a nobis eum doloribus officia itaque commodi perferendis voluptates quae, dolorum ex deserunt.',
+            price  //shorthand do not need price: price 
         });
         await camp.save()
     }
